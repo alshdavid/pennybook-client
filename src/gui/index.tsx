dbec887e4b28cc7e8a506559ecc215d2fcec79a8
David Alsh <alshdavid@gmail.com>
2025-11-02 12:00:04 +1100

    SQL UI

--- src/gui/index.tsx
+++ src/gui/index.tsx
@@ -1,6 +1,7 @@
 import "./styles.css";
-import { h, render } from "preact";
+import { h, render, type TargetedKeyboardEvent } from "preact";
 import { SharedWorker } from "@alshdavid/shared-worker/shared-worker.ts";
+import { useState } from "preact/hooks";
 
 const worker = new SharedWorker(globalThis.importMap.resolve("worker")!, {
   type: "module",
@@ -29,48 +30,79 @@ function exec(sql: string) {
   return result;
 }
 
-async function sql(template: TemplateStringsArray) {
-  return exec(template[0]);
-}
-
 // @ts-expect-error
 globalThis.exec = exec;
 
-await sql`CREATE TABLE IF NOT EXISTS test_table ("id" TEXT UNIQUE, "val" TEXT);`;
-
-// await exec("INSERT INTO test_table (id, val) VALUES ('1', 'v1')")
-// await exec("INSERT INTO test_table (id, val) VALUES ('2', 'v2')")
-// await exec("INSERT INTO test_table (id, val) VALUES ('3', 'v3')")
-
-console.log(await sql`SELECT * FROM test_table;`);
-
 function App() {
+  const [loading, setLoading] = useState(false)
+  const [query, setQuery] = useState('SELECT * FROM test_table;')
+  const [result, setResult] = useState([])
+  const [error, setError] = useState<null | string>(null)
+  const rows = rowsToTable(result)
+
+  async function submit() {
+    if (loading) {
+      return
+    }
+    setLoading(true)
+    setError(null)
+    setResult([])
+
+    try {
+      const result = await exec(query)
+      setResult(result)
+      setQuery('')
+    } catch (error: any) {
+      setError(error.message)
+    }
+
+    setLoading(false)
+  }
+
+  function onEnter(event: TargetedKeyboardEvent<HTMLTextAreaElement>) {
+    if (event.key === 'Enter' || event.keyCode === 13) {
+        submit()
+    }
+  }
+
   return (
     <div>
-      <h1>Website Template</h1>
-      <p>Basic starter template for bundled websites using Preact</p>
-      <p>
-        <b>It Features;</b>
-      </p>
-      <ul>
-        <li>Rspack bundler</li>
-        <li>TypeScript</li>
-        <li>Preact</li>
-        <li>CSS Modules/Nesting</li>
-        <li>Web Workers</li>
-        <li>Bundle Size Stats</li>
-        <li>Workflow to publish to GitHub Pages</li>
-        <li>
-          Template distributes 5kb of content (Preact, CSS, Worker + App code)
-        </li>
-      </ul>
-      <div>
-        <a href="https://github.com/alshdavid-templates/website-template">
-          GitHub Repo
-        </a>
-      </div>
+      <textarea
+        disabled={loading}
+        placeholder="Write SQL here"
+        onKeyDown={onEnter}
+        value={query}
+        onInput={(e: any) => setQuery(e.target.value)} />
+
+      <button 
+        disabled={loading}
+        onClick={submit}>Exec</button>
+
+      <h1>Result:</h1>
+
+      {error && <div>Error: {error}</div>}
+      {rows.length ? <table>
+        {rows.map(entry => <tr>
+          {entry.map(key => <td>{key}</td>)}
+        </tr>)}
+      </table> : null}
     </div>
   );
 }
 
 render(<App />, document.body);
+
+
+function rowsToTable<T extends Record<string, any>>(rows: T[]): string[][] {
+  if (rows.length === 0) {
+    return [];
+  }
+  
+  const headers = Object.keys(rows[0]);
+  
+  const values = rows.map(row => 
+    headers.map(header => String(row[header]))
+  );
+  
+  return [headers, ...values];
+}
