01b5f60b7155d60bfdcd42981caa60eed7a5be92
David Alsh <alshdavid@gmail.com>
2026-04-11 13:33:47 +1000

    small fix

--- src/sections/page-body/page-body.tsx
+++ src/sections/page-body/page-body.tsx
@@ -1,12 +1,9 @@
 import "./page-body.scss";
 import { Fragment, h } from "preact";
-import { SideBarLeft } from "../side-bar-left/side-bar-left.tsx";
 
 export function PageBody({ children = null }: { children: any }) {
   return (
     <Fragment>
-      {/* <SideBarLeft /> */}
-
       <main className="section-page-body">
         <section className="main-contents-body">{children}</section>
       </main>
